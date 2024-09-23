4cb21d553d7047a7b69feaf2527449eebcc23c1e
MatveyMironov <matveymironof@gmail.com>
2024-09-23 13:35:05 +0300

    Reapply "Better tracking"
    This reverts commit 9b0079ed457b80966b9dc21c212e2e8fadcfd5fe.

--- TowerDefense/Assets/Features/Turrets/TargetTracking.cs
+++ TowerDefense/Assets/Features/Turrets/TargetTracking.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 [Serializable]
 public class TargetTracking
 {
-    [field: Tooltip("The part that aims at the target")]
-    [field: SerializeField] public Transform Weapon { get; private set; }
-    [field: SerializeField] public float RotationSpeed { get; private set; }
+    [Tooltip("The part that aims at the target")]
+    [SerializeField] private Transform muzzle;
+    [Tooltip("The part that rotates in horizontal plane")]
+    [SerializeField] private Transform horizontalHinge;
+    [SerializeField] private float horizontalRotationSpeed;
+    [Tooltip("The part that rotates in vertical plane")]
+    [SerializeField] private Transform verticalHinge;
+    [SerializeField] private float verticalRotationSpeed;
 
     public void TrackTarget(Transform target)
     {
         if (target == null)
             return;
 
-        Quaternion lookRotation = Quaternion.LookRotation(target.transform.position - Weapon.position);
-        Quaternion targetRotation = new Quaternion(0f, lookRotation.y, 0f, lookRotation.w);
+        Quaternion lookRotation = Quaternion.LookRotation(target.transform.position - muzzle.position);
+        Vector3 rotationEuler = lookRotation.eulerAngles;
+        Quaternion horizontalRotation = Quaternion.Euler(0f, rotationEuler.y, 0f);
+        Quaternion verticalRotation = Quaternion.Euler(rotationEuler.x, 0f, 0f);
 
-        if (Weapon.rotation != lookRotation)
+        if (muzzle.rotation != lookRotation)
         {
-            Weapon.rotation = Quaternion.RotateTowards(Weapon.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+            horizontalHinge.localRotation = Quaternion.RotateTowards(horizontalHinge.localRotation, horizontalRotation, horizontalRotationSpeed * Time.deltaTime);
+            verticalHinge.localRotation = Quaternion.RotateTowards(verticalHinge.localRotation, verticalRotation, verticalRotationSpeed * Time.deltaTime);
         }
     }
 }
